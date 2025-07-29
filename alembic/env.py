9e7d328f1bee42a8fd286641dcf10dadb4233bd4
cretcheu <adalio.semedo@my.istec.pt>
2025-07-29 11:05:19 +0100

    Ajusta sys.path no alembic/env.py para importar hora_certa_app

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,9 @@ from sqlalchemy import engine_from_config, pool
 from alembic import context
 from dotenv import load_dotenv
 
-# Ajusta o path para permitir import de hora_certa_app
+# ====================================================================
+# 1) Garanta que o diretório raiz do seu projeto esteja no Python path
+# ====================================================================
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 # carrega variáveis de ambiente de .env (local) ou do Railway
@@ -16,14 +18,14 @@ from hora_certa_app.db import Base
 import hora_certa_app.models.user
 import hora_certa_app.models.appointment
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+# this is the Alembic Config object, que fornece
+# acesso às configurações no alembic.ini
 config = context.config
 
 # Override da URL de conexão usando a variável de ambiente
 config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
 
-# Interpret the config file for Python logging.
+# Configura logging segundo o alembic.ini
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
@@ -32,7 +34,7 @@ target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode."""
+    """Run migrations em modo 'offline'."""
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -40,22 +42,19 @@ def run_migrations_offline() -> None:
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
     )
-
     with context.begin_transaction():
         context.run_migrations()
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode."""
+    """Run migrations em modo 'online'."""
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
-
     with connectable.connect() as connection:
         context.configure(connection=connection, target_metadata=target_metadata)
-
         with context.begin_transaction():
             context.run_migrations()
 
